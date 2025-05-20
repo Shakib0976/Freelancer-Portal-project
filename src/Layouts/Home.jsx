7dc1b978614ca769d62473a8c2bd3119c2185a6f
Shakib0976 <web.shakib09@gmail.com>
2025-05-20 19:44:26 +0600

    responsive home page

--- src/Layouts/Home.jsx
+++ src/Layouts/Home.jsx
@@ -7,8 +7,8 @@ import { Typewriter } from 'react-simple-typewriter';
 const Home = () => {
     return (
         <Carousel showThumbs={false} autoPlay infiniteLoop>
-            <div className='text-start py-20 md:py-30 bg-cover bg-center  px-10 md:px-20 text-white space-y-8' style={{ backgroundImage: "url('/i2.jpg')" }} >
-                <h1 className=' text-3xl md:text-5xl font-bold'><span className='text-green-500'>Find</span> <span className=''>Expert</span> <span><Typewriter
+            <div className='text-start py-20 md:py-30 bg-cover bg-center  px-5 md:px-20 text-white space-y-2 md:space-y-8' style={{ backgroundImage: "url('/i2.jpg')" }} >
+                <h1 className=' text-xl md:text-5xl font-bold'><span className='text-green-500'>Find</span> <span className=''>Expert</span> <span><Typewriter
                     words={['Developer', 'Designer', 'Creator', 'programmer']}
                     loop={5}
                     cursor
@@ -20,8 +20,8 @@ const Home = () => {
                 <p className='md:text-xl'>Connect with skilled professionals ready  to tackle your <br /> projects quickly and efficiently.</p>
                 <button className='btn btn-primary'>Post a Task</button>
             </div>
-            <div className='text-start py-30 bg-cover bg-center px-20 text-white space-y-8' style={{ backgroundImage: "url('/s3.jpg')" }} >
-                <h1 className='text-5xl font-bold'><span><Typewriter
+            <div className='text-start py-20 md:py-30 bg-cover bg-center  px-5 md:px-20 text-white space-y-2 md:space-y-8' style={{ backgroundImage: "url('/s3.jpg')" }} >
+                <h1 className='text-xl md:text-5xl font-bold'><span><Typewriter
                     words={['Connect with Clients Worldwide', 'Join the Freelance Marketplace', 'Find Work That Fits Your Skills', 'Work Remotely on Your Terms']}
                     loop={5}
                     cursor
@@ -30,11 +30,11 @@ const Home = () => {
                     deleteSpeed={50}
                     delaySpeed={1000}
                 /></span></h1>
-                <p className='text-xl'>Connect with skilled professionals ready  to tackle your <br /> projects quickly and efficiently.</p>
+                <p className='md:text-xl'>Connect with skilled professionals ready  to tackle your <br /> projects quickly and efficiently.</p>
                 <button className='btn btn-primary hover:btn-secondary'>Brows a task</button>
             </div>
-            <div className='text-start py-30 bg-cover bg-center px-20 text-white space-y-8' style={{ backgroundImage: "url('/s2.jpg')" }} >
-                <h1 className='text-5xl font-bold'><span className='text-green-500'>Work on</span> <span><Typewriter
+            <div className=' text-start py-20 md:py-30 bg-cover bg-center  px-5 md:px-20 text-white space-y-2 md:space-y-8' style={{ backgroundImage: "url('/s2.jpg')" }} >
+                <h1 className='text-xl md:text-5xl font-bold'><span className='text-green-500'>Work on</span> <span><Typewriter
                     words={['Business Plan', 'E-commerce Store', 'Website', 'personal Branding']}
                     loop={5}
                     cursor
@@ -43,7 +43,7 @@ const Home = () => {
                     deleteSpeed={50}
                     delaySpeed={1000}
                 /></span></h1>
-                <p className='text-xl'>Set your own schedule, choose your projects, <br /> and build your freelance career.</p>
+                <p className='md:text-xl'>Set your own schedule, choose your projects,  and build your freelance career. and successfully</p>
                 <button className='btn btn-primary'>Post a Task</button>
             </div>
         </Carousel>
