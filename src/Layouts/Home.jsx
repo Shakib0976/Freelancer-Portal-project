c2cfce71dff95d0a5de744c0da8ef1a67c2364d1
Shakib0976 <web.shakib09@gmail.com>
2025-05-20 18:58:56 +0600

    added responsive home page

--- src/Layouts/Home.jsx
+++ src/Layouts/Home.jsx
@@ -6,9 +6,9 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 const Home = () => {
     return (
         <Carousel showThumbs={false} autoFocus infiniteLoop>
-            <div className='text-start py-30 bg-cover bg-center px-20 text-white space-y-8' style={{ backgroundImage: "url('/i2.jpg')" }} >
-                <h1 className='text-5xl font-bold'><span className='text-green-500'>Find</span> <span className=''>Expert</span> Freelancers</h1>
-                <p className='text-xl'>Connect with skilled professionals ready  to tackle your <br /> projects quickly and efficiently.</p>
+            <div className='text-start py-20 md:py-30 bg-cover bg-center  px-10 md:px-20 text-white space-y-8' style={{ backgroundImage: "url('/i2.jpg')" }} >
+                <h1 className=' text-3xl md:text-5xl font-bold'><span className='text-green-500'>Find</span> <span className=''>Expert</span> Freelancers</h1>
+                <p className='md:text-xl'>Connect with skilled professionals ready  to tackle your <br /> projects quickly and efficiently.</p>
                 <button className='btn btn-primary'>Post a Task</button>
             </div>
             <div className='text-start py-30 bg-cover bg-center px-20 text-white space-y-8' style={{ backgroundImage: "url('/s2.jpg')" }} >
@@ -19,7 +19,7 @@ const Home = () => {
             <div className='text-start py-30 bg-cover bg-center px-20 text-white space-y-8' style={{ backgroundImage: "url('/s3.jpg')" }} >
                 <h1 className='text-5xl font-bold'><span className='text-green-500'>Find</span> <span className=''>Expert</span> Freelancers</h1>
                 <p className='text-xl'>Connect with skilled professionals ready  to tackle your <br /> projects quickly and efficiently.</p>
-                <button className='btn btn-primary'>Post a Task</button>
+                <button className='btn btn-primary hover:btn-secondary'>Brows a task</button>
             </div>
         </Carousel>
     );
