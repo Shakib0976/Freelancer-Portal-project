5c37b419c60eed9f9883007a46f6dfd549b4f528
Shakib0976 <web.shakib09@gmail.com>
2025-05-21 18:36:26 +0600

    card added and bounce effect added

---
+++ src/Layouts/CardAnnimation.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import { motion } from "framer-motion";
+
+const CardAnnimation = () => {
+    return (
+        <div className='w-11/12 mx-auto my-7'>
+            <h1 className='md:text-5xl text-3xl  mt-20 mb-8 font-semibold'>Popular services</h1>
+
+            <div className='grid  md:grid-cols-2 lg:grid-cols-6'>
+                <div className='m-5'>
+                    <motion.div
+                        whileHover={{ scale: 1.1 }}
+                        whileInView={{ y: [50, 0], opacity: [0, 1] }}
+                        transition={{ type: "spring", bounce: 0.6, duration: 0.6 }}
+                    >
+                        <div className='bg-[#0D4715] p-3 rounded-2xl h-65'>
+                            <h1 className=' text-white mt-2 mb-12 font-semibold text-xl '>Architecture & Interior Design</h1>
+                            <img className='rounded-2xl md:h-30 w-full' src="/c7.jpg" alt="" />
+
+                        </div>
+
+                    </motion.div>
+                </div>
+
+
+                <div className='m-5'>
+                    <motion.div
+                        whileHover={{ scale: 1.1 }}
+                        whileInView={{ y: [50, 0], opacity: [0, 1] }}
+                        transition={{ type: "spring", bounce: 0.6, duration: 0.6 }}
+                    >
+                        <div className='bg-[#0D4715] p-3 rounded-2xl h-65'>
+                            <h1 className='  text-white mt-2 mb-12 font-semibold text-xl '>Software Development</h1>
+                            <img className='rounded-2xl md:h-30 w-full' src="/c2.jpg" alt="" />
+
+                        </div>
+
+                    </motion.div>
+                </div>
+
+
+
+                <div className='m-5'>
+                    <motion.div
+                        whileHover={{ scale: 1.1 }}
+                        whileInView={{ y: [50, 0], opacity: [0, 1] }}
+                        transition={{ type: "spring", bounce: 0.6, duration: 0.6 }}
+                    >
+                        <div className='bg-[#0D4715]  p-3 rounded-2xl h-65'>
+                            <h1 className=' text-white mt-2 mb-19 font-semibold text-xl '>Video Editing</h1>
+                            <img className='rounded-2xl md:h-30 w-full' src="/c3.jpg" alt="" />
+
+                        </div>
+
+                    </motion.div>
+                </div>
+
+
+
+                <div className='m-5'>
+                    <motion.div
+                        whileHover={{ scale: 1.1 }}
+                        whileInView={{ y: [50, 0], opacity: [0, 1] }}
+                        transition={{ type: "spring", bounce: 0.6, duration: 0.6 }}
+                    >
+                        <div className='bg-[#0D4715] p-3 rounded-2xl h-65'>
+                            <h1 className=' text-white mt-2 mb-12 font-semibold text-xl '>Website Development</h1>
+                            <img className='rounded-2xl md:h-30 w-full' src="/c4.jpg" alt="" />
+
+                        </div>
+
+                    </motion.div>
+                </div>
+
+
+
+                <div className='m-5'>
+                    <motion.div
+                        whileHover={{ scale: 1.1 }}
+                        whileInView={{ y: [50, 0], opacity: [0, 1] }}
+                        transition={{ type: "spring", bounce: 0.6, duration: 0.6 }}
+                    >
+                        <div className='bg-[#0D4715] p-3 rounded-2xl h-65'>
+                            <h1 className=' text-white mt-2 mb-19 font-semibold text-xl '>AI Development</h1>
+                            <img className='rounded-2xl h-30 w-full ' src="/c5.jpg" alt="" />
+
+                        </div>
+
+                    </motion.div>
+                </div>
+
+
+
+                <div className='m-5'>
+                    <motion.div
+                        whileHover={{ scale: 1.1 }}
+                        whileInView={{ y: [50, 0], opacity: [0, 1] }}
+                        transition={{ type: "spring", bounce: 0.6, duration: 0.6 }}
+                    >
+                        <div className='bg-[#0D4715] p-3 rounded-2xl h-65'>
+                            <h1 className=' text-white mt-2 mb-19 font-semibold text-xl  '>SEO</h1>
+                            <img className='rounded-2xl h-30 w-full' src="/c6.jpg" alt="" />
+
+                        </div>
+
+                    </motion.div>
+                </div>
+
+
+            </div>
+
+
+        </div>
+    );
+};
+
+export default CardAnnimation;
--- src/Layouts/Home.jsx
+++ src/Layouts/Home.jsx
@@ -1,52 +1,15 @@
 import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Typewriter } from 'react-simple-typewriter';
+import Carusel from './Carusel';
+import CardAnnimation from './CardAnnimation';
+
 
 
 const Home = () => {
     return (
-        <Carousel showThumbs={false} autoPlay infiniteLoop>
-            <div className=' text-start py-20 md:py-40 bg-cover bg-center  px-5 md:px-20 text-white space-y-2 md:space-y-8' style={{ backgroundImage: "url('/i2.jpg')" }} >
-                <h1 className=' text-xl md:text-4xl lg:text-6xl font-bold'><span className='text-green-500'>Find</span> <span className=''>Expert</span> <span><Typewriter
-                    words={['Developer', 'Designer', 'Creator', 'programmer']}
-                    loop={5}
-                    cursor
-                    cursorStyle="|"
-                    typeSpeed={70}
-                    deleteSpeed={50}
-                    delaySpeed={1000}
-                /></span></h1>
-                <p className='md:text-xl'>Connect with skilled professionals ready  to tackle your <br /> projects quickly and efficiently.</p>
-                <button className='btn btn-primary'>Post a Task</button>
-            </div>
-            <div className='text-start py-20 md:py-40 bg-cover bg-center  px-5 md:px-20 text-white space-y-2 md:space-y-8' style={{ backgroundImage: "url('/s3.jpg')" }} >
-                <h1 className='text-xl md:text-4xl lg:text-6xl font-bold'><span><Typewriter
-                    words={['Connect with Clients Worldwide', 'Join the Freelance Marketplace', 'Find Work That Fits Your Skills', 'Work Remotely on Your Terms']}
-                    loop={5}
-                    cursor
-                    cursorStyle="|"
-                    typeSpeed={70}
-                    deleteSpeed={50}
-                    delaySpeed={1000}
-                /></span></h1>
-                <p className='md:text-xl'>Connect with skilled professionals ready  to tackle your <br /> projects quickly and efficiently.</p>
-                <button className='btn btn-primary hover:btn-secondary'>Brows a task</button>
-            </div>
-            <div className=' text-start py-20 md:py-40 bg-cover bg-center  px-5 md:px-20 text-white space-y-2 md:space-y-8' style={{ backgroundImage: "url('/s2.jpg')" }} >
-                <h1 className='text-xl md:text-4xl lg:text-6xl font-bold'><span className='text-green-500'>Work on</span> <span><Typewriter
-                    words={['Business Plan', 'E-commerce Store', 'Website', 'personal Branding']}
-                    loop={5}
-                    cursor
-                    cursorStyle="|"
-                    typeSpeed={70}
-                    deleteSpeed={50}
-                    delaySpeed={1000}
-                /></span></h1>
-                <p className='md:text-xl'>Set your own schedule, choose your projects,  and build your <br /> freelance career. and successfully</p>
-                <button className='btn btn-primary'>Post a Task</button>
-            </div>
-        </Carousel>
+       <div>
+        <Carusel></Carusel>
+        <CardAnnimation></CardAnnimation>
+       </div>
     );
 };
 
